6b9b852dd2a9da43ab80a1b0a145ebd9639f86c7
Celina <luv.mins99@gmail.com>
2022-04-16 23:30:38 +0900

    feat: 입력 값 정보 도메인 Number 생성

---
+++ src/main/java/baseball/Model/Number.java
@@ -0,0 +1,42 @@
+package baseball.Model;
+
+import java.util.Objects;
+
+public class Number {
+    private final int number;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final String NUMBER_ERROR_MESSAGE = "1이상 9이하의 숫자만 입력가능합니다.";
+
+    public Number(int number) {
+        invalidNumberCheck(number);
+        this.number = number;
+    }
+
+    private void invalidNumberCheck(int number) {
+        if (number < MIN_NUMBER || number > MAX_NUMBER) {
+            throw new IllegalArgumentException(NUMBER_ERROR_MESSAGE);
+        }
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Number number1 = (Number) o;
+        return number == number1.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+}
---
+++ src/test/java/baseball/NumberTest.java
@@ -0,0 +1,32 @@
+package baseball;
+
+import baseball.Model.Number;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+public class NumberTest {
+    @ParameterizedTest
+    @CsvSource({"1", "2", "3", "4", "5", "6", "7", "8", "9"})
+    void 숫자_객체는_1에서_9사이의_수를_가지고있다(int input) {
+        // given
+        Number number = new Number(input);
+
+        // when
+        int output = number.getNumber();
+
+        // then
+        Assertions.assertThat(input).isEqualTo(output);
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0", "10"})
+    void 숫자_객체는_1에서_9사이의_수가_아니면_오류를_발생시킨다(int input) {
+        // given & when & then
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new Number(input)
+        );
+    }
+}
