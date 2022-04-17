1b133751e7ade6c454b0b6b667eb4e310b404afb
Celina <luv.mins99@gmail.com>
2022-04-18 00:20:45 +0900

    feat: 컴퓨터의 입력 값을 생성하는 RandomGenerator 생성

---
+++ src/main/java/baseball/utils/RandomGenerator.java
@@ -0,0 +1,20 @@
+package baseball.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.LinkedHashSet;
+
+public class RandomGenerator {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final String DELIMITER_VALUE = "";
+
+    public static String create() {
+        LinkedHashSet<String> inputValue = new LinkedHashSet<>();
+        while (inputValue.size() != 3) {
+            inputValue.add(String.valueOf(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER)));
+        }
+
+        return String.join(DELIMITER_VALUE, inputValue);
+    }
+}
