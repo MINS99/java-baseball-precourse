935eca55881daa0b5bc8cd68ecbf53aa3d2ddc03
Celina <luv.mins99@gmail.com>
2022-04-18 00:39:15 +0900

    feat: 숫자 야구 게임 실행과 관련된 Controller 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.controller.BaseballGameController;
+import baseball.view.GameInput;
+import baseball.view.GameOutput;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGameController gameController = new BaseballGameController(new GameInput(), new GameOutput());
+        gameController.startGame();
     }
 }
---
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -0,0 +1,37 @@
+package baseball.controller;
+
+import baseball.model.BallStatus;
+import baseball.model.Game;
+import baseball.model.GameStatus;
+import baseball.view.GameInput;
+import baseball.view.GameOutput;
+import baseball.utils.RandomGenerator;
+
+import java.util.List;
+
+public class BaseballGameController {
+    private final GameInput gameInput;
+    private final GameOutput gameOutput;
+
+    public BaseballGameController(GameInput gameInput, GameOutput gameOutput) {
+        this.gameInput = gameInput;
+        this.gameOutput = gameOutput;
+    }
+
+    public void startGame() {
+        Game game = new Game();
+        while (game.getGameStatus() != GameStatus.END) {
+            play(game);
+        }
+    }
+
+    private void play(Game game) {
+        game.start(RandomGenerator.create());
+        while (game.getGameStatus() != GameStatus.FINISHED) {
+            List<BallStatus> ballStatusList = game.input(gameInput.input());
+            gameOutput.viewBallStatus(ballStatusList);
+        }
+        gameOutput.printEndResult();
+        game.end(gameInput.restart());
+    }
+}
